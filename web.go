82d1f1300af4eb5eaefa3a2b422331d14a79afdd
liz <liz@heroku.com>
2014-10-10 11:43:03 -0700

    Added one year ago functionality

--- web.go
+++ web.go
@@ -6,7 +6,13 @@ import (
     "os"
     "log"
     "fmt"
+    "bufio"
+    "regexp"
+    "strconv"
 )
+
+const timeFormat = "20060102"
+var getDT = regexp.MustCompile(`^(DT(?:START|END):)(\d{4})(.*)?$`)
 
 func timewarpServer(w http.ResponseWriter, req *http.Request) {
     values, err := url.ParseQuery(req.URL.RawQuery)
@@ -23,7 +29,30 @@ func timewarpServer(w http.ResponseWriter, req *http.Request) {
         return
     }
 
-    fmt.Fprintf(w, "You submitted %v\n", ical)
+    resp, err := http.Get(ical)
+    defer resp.Body.Close()
+    if err != nil {
+        log.Printf("http error %v\n", err)
+        w.WriteHeader(502)
+        fmt.Fprintln(w, "couldn't retrieve your URL")
+        return
+    }
+
+    w.Header().Add("Content-Type", "text/calendar")
+    scanner := bufio.NewScanner(resp.Body)
+    for scanner.Scan() {
+        line := scanner.Text()
+        match := getDT.FindStringSubmatch(line)
+        if match != nil {
+            i, err := strconv.Atoi(match[2])
+            if err == nil {
+                // One year in future
+                fmt.Fprintf(w, "%s%d%s\r\n", match[1], i + 1, match[3])
+                continue
+            }
+        }
+        fmt.Fprintln(w, line)
+    }
 }
 
 func main() {
